3a9ff988b1e709d2e2336641a652041a58418374
droare <droare@gmail.com>
2024-12-30 23:41:53 -0600

    1

---
+++ pythonProject1/PrimeraClase.py
@@ -0,0 +1,10 @@
+#Concatenacion de funciones
+
+print('Tu nombre es ' + input("Introduce tu nombre: ") + " " + input("Introduce tu apellido: "))
+
+#Ejercicio 1
+
+
+print("El nombre de tu vino es: " + input("Introduce el tipo de vino: ") + " " + input("Introduce la region del vino: "))
+
+
